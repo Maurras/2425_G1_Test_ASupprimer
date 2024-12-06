72b5efeb1816b6cf75687864bf1536b76efe86ab
MaurrasT <64419095+Maurras@users.noreply.github.com>
2024-12-06 10:16:29 +0100

    MT : Création du projet

---
+++ PhpProject1/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Test Github</title>
+    </head>
+    <body>
+        <?php
+            echo 'Mon test Git';
+        ?>
+    </body>
+</html>
